27a004208ccbe0af2d39ebc3dc155515a5d8fe7d
Ntandoyenkosi Zungu <166859858+CelestialLelouch@users.noreply.github.com>
2025-11-19 16:58:21 +0200

    First commit

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Container } from '@mui/material'
+
+
+//Desktop Looking portfolio
+
+function App() {
+
+  return (
+    <Container>
+      Hello World
+    </Container>
+  )
+}
+
+export default App
